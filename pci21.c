3e465a5096d8e50369f69e91ead09d63d2aa26ec
MmAaXx500 <viktor.balogh45@gmail.com>
2025-06-04 18:58:50 +0200

    implement basic PCI device enumeration

---
+++ pci21.c
@@ -0,0 +1,75 @@
+#include "pci21.h"
+#include "io.h"
+
+// PCI Configuration Space Access Mechanism #1 IO locations
+#define PCI_CONFIG_ADDRESS 0xcf8
+#define PCI_CONFIG_DATA 0xcfc
+
+static uint32_t pci_read_dword(const uint8_t bus, const uint8_t device,
+                               const uint8_t func, const uint8_t offset) {
+
+	uint8_t reg_num = offset >> 2;
+	uint32_t addr = 0;
+	addr |= (uint32_t)1 << 31;               // bit 31 enable bit
+	                                         // bits 24-30 reserved (0)
+	addr |= (uint32_t)bus << 16;             // bits 16-23
+	addr |= (uint32_t)(device & 0x1f) << 11; // bits 11-15
+	addr |= (uint32_t)(func & 0x07) << 8;    // bits 8-10
+	addr |= (uint32_t)reg_num << 2;          // bits 2-7
+	// bits 0-1 is 0 for 32bit aligned access
+
+	outl(PCI_CONFIG_ADDRESS, addr);
+
+	return inl(PCI_CONFIG_DATA);
+}
+
+static uint16_t pci_read_word(const uint8_t bus, const uint8_t device,
+                              const uint8_t func, const uint8_t offset) {
+	uint32_t tmp = pci_read_dword(bus, device, func, offset);
+	return (tmp >> ((offset & 0x02) * 8)) & 0xffff;
+}
+
+static uint8_t pci_read_byte(const uint8_t bus, const uint8_t device,
+                             const uint8_t func, const uint8_t offset) {
+	uint32_t tmp = pci_read_dword(bus, device, func, offset);
+	return (tmp >> ((offset & 0x03) * 8)) & 0xff;
+}
+
+static void pci_read_device(const uint8_t bus, const uint8_t device,
+                            struct pci_header *header, pci_device_cb_t cb,
+                            void *userdata) {
+	uint8_t max_funcs = 1;
+
+	header->vendor_id = pci_read_word(bus, device, 0, 0);
+	if (header->vendor_id == 0xffff)
+		return;
+
+	header->header_type = pci_read_byte(bus, device, 0, 0xe);
+	if ((header->header_type & 0x80) != 0)
+		max_funcs = 8;
+
+	for (uint8_t func = 0; func < max_funcs; func++) {
+		header->device_id = pci_read_word(bus, device, func, 0x2);
+		header->command = pci_read_word(bus, device, func, 0x4);
+		header->status = pci_read_word(bus, device, func, 0x6);
+		header->rev_id = pci_read_byte(bus, device, func, 0x8);
+		header->prog_if = pci_read_byte(bus, device, func, 0x9);
+		header->subclass = pci_read_byte(bus, device, func, 0xa);
+		header->class_code = pci_read_byte(bus, device, func, 0xb);
+		header->cache_line_size = pci_read_byte(bus, device, func, 0xc);
+		header->latency_timer = pci_read_byte(bus, device, func, 0xd);
+		header->bist = pci_read_byte(bus, device, func, 0xf);
+
+		cb(bus, device, func, header, userdata);
+	}
+}
+
+void pci_enumerate_devices(pci_device_cb_t cb, void *userdata) {
+	struct pci_header header = {0};
+
+	for (uint16_t bus = 0; bus < 256; bus++) {
+		for (uint8_t device = 0; device < 32; device++) {
+			pci_read_device((uint8_t)bus, device, &header, cb, userdata);
+		}
+	}
+}
---
+++ pci21.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <stdint.h>
+
+struct pci_header {
+	uint16_t device_id;
+	uint16_t vendor_id;
+	uint16_t status;
+	uint16_t command;
+	uint8_t rev_id;
+	uint8_t prog_if;
+	uint8_t subclass;
+	uint8_t class_code;
+	uint8_t cache_line_size;
+	uint8_t latency_timer;
+	uint8_t header_type;
+	uint8_t bist;
+};
+
+typedef void (*pci_device_cb_t)(const uint8_t bus, const uint8_t device,
+                                const uint8_t func,
+                                const struct pci_header *dev_header,
+                                void *userdata);
+
+void pci_enumerate_devices(pci_device_cb_t cb, void *userdata);
