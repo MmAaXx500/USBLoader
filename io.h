7169ce02e44187c458de442a396087feddd530ab
MmAaXx500 <viktor.balogh45@gmail.com>
2025-06-07 21:42:51 +0200

    add inw, outw

--- io.h
+++ io.h
@@ -8,6 +8,13 @@ static inline void outb(uint16_t port, uint8_t val) {
 	                 : "memory");
 }
 
+static inline void outw(uint16_t port, uint16_t val) {
+	__asm__ volatile("outw %w0, %w1"
+	                 :
+	                 : "a"(val), "Nd"(port)
+	                 : "memory");
+}
+
 static inline void outl(uint16_t port, uint32_t val) {
 	__asm__ volatile("outl %k0, %w1"
 	                 :
@@ -18,6 +25,15 @@ static inline void outl(uint16_t port, uint32_t val) {
 static inline uint8_t inb(uint16_t port) {
 	uint8_t ret;
 	__asm__ volatile("inb %w1, %b0"
+	                 : "=a"(ret)
+	                 : "Nd"(port)
+	                 : "memory");
+	return ret;
+}
+
+static inline uint16_t inw(uint16_t port) {
+	uint16_t ret;
+	__asm__ volatile("inw %w1, %w0"
 	                 : "=a"(ret)
 	                 : "Nd"(port)
 	                 : "memory");
